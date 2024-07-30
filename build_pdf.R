b211850da6fa5bac689b3851eedddb745d5acc31
Brian Williamson <brian.d.williamson@kp.org>
2024-07-30 10:58:05 -0700

    Add short version of slides for 10 min talk

--- build_pdf.R
+++ build_pdf.R
@@ -2,3 +2,6 @@
 library("renderthis")
 renderthis::to_pdf(from = "index.Rmd", to = "cluster_hte.pdf",
                    complex_slides = TRUE, partial_slides = TRUE)
+
+renderthis::to_pdf(from = "cluster_hte_short.Rmd", to = "cluster_hte_short.pdf",
+                   complex_slides = TRUE, partial_slides = TRUE)
